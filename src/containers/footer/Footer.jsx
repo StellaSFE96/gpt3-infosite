db617b3a64a34d7b54774f5696a0247e27e74eaa
StellaSFE96 <stella.samaranayake@iths.se>
2023-09-04 18:28:07 +0200

    fix image issue with responsivity in possibilities section by removing height value

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => (
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
     </div>
 
-    <div className="gpt3__footer-btn">
+    <button>
       <p>Request Early Access</p>
-    </div>
+    </button>
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
