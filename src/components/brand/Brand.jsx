91d34b59d0d25e1ec1b1acf0e33a015aac5217af
StellaSFE96 <stella.samaranayake@iths.se>
2023-09-04 17:32:57 +0200

    add header, brands, feature and whatGPT3 sections with styles

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './brand.css'
+import React from 'react';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './brand.css';
 
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt='google icon' />
+    </div>
+    <div>
+      <img src={slack} alt='slack icon'/>
+    </div>
+    <div>
+      <img src={atlassian} alt='atlassian icon'/>
+    </div>
+    <div>
+      <img src={dropbox} alt='dropbox icon'/>
+    </div>
+    <div>
+      <img src={shopify} alt='shopify icon'/>
+    </div>
+  </div>
+);
 
 export default Brand;
-
